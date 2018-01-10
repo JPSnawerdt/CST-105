31f454f49141e2c463e3e076f5680286a4d7a00f
jps5y <jpsnawerdt@gmail.com>
2018-01-10 14:24:51 -0500

    Week 1 Assignments
    My Name is John Snawerdt and this work is my own.

---
+++ Week 1/Introduction.java
@@ -0,0 +1,12 @@
+/**
+ * My name is John Snawerdt and all of this work is my own.
+ */
+public class Introduction {
+	public static void main(String[] args) {
+		System.out.println("My Name is John Snawerdt");
+		System.out.println("My Favorite Color is Blue");
+		System.out.println("If I won the lottery I would use it to pay off my mortgage.");
+		
+	}
+
+}
